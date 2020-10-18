49ef8d4ae4b920c05d6bdef74fedbe4b3a774521
lfgribeio <telu@1983>
2020-10-18 16:59:40 -0300

    Criado atributo de classe

--- oo/Pessoa.py
+++ oo/Pessoa.py
@@ -1,18 +1,26 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=35):
+    olhos = 2
+
+    def __init__(self, *filhos,nome=None, idade=35):
         self.idade = idade
         self.nome = nome
-
+        self.filhos = list(filhos)
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa("Luis")
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Filippe'
-    print(p.nome)
-    print(p.idade)
-
+    leticia = Pessoa("Letícia")
+    luis = Pessoa(leticia, nome='Luis')
+    print(Pessoa.cumprimentar(luis))
+    print(id(luis))
+    print(luis.cumprimentar())
+    print(luis.nome)
+    luis.nome = 'Filippe'
+    print(luis.nome)
+    print(luis.idade)
+    for filho in luis.filhos:
+        print(filho.nome)
+    luis.sobrenome = 'Ribeiro'
+    del luis.filhos
+    print(luis.__dict__)
+    print(leticia.__dict__)
